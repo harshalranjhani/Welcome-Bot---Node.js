ff03b17fc469cfb937e9972fd5db9753d5ef499d
Harshal <ranjhaniharshal@gmail.com>
2022-04-21 18:09:47 +0530

    add embeds

--- commands.js
+++ commands.js
@@ -1,22 +1,35 @@
-const bot = require('./commands/welcome.js')
-const gif = require('./commands/gif.js');
-const help = require('./commands/help.js');
+const bot = require("./commands/welcome.js");
+const gif = require("./commands/gif.js");
+const help = require("./commands/help.js");
 
 const commands = { bot, gif, help };
 
 module.exports = async function (msg) {
-    // console.log(msg.content)
-    if (msg.channel.id === '962242299226628097') {
-        let tokens = msg.content.split(' ');
-        let command = tokens.shift(); // removes the first element and shifts the array accordingly
-        try {
-            if (command.charAt(0) === '!') {
-                command = command.substring(1);
-                commands[command](msg, tokens); // here tokens is the argument
-                // valid command
-            }
-        } catch (e) {
-            console.log("Not a valid command!");
-        }
+  // console.log(msg.content)
+  // so that it runs in a particular channel in my server
+  if (msg.channel.id === "962242299226628097") {
+    let tokens = msg.content.split(" ");
+    let command = tokens.shift(); // removes the first element and shifts the array accordingly
+    try {
+      if (command.charAt(0) === "!") {
+        command = command.substring(1);
+        commands[command](msg, tokens); // here tokens is the argument
+        // valid command
+      }
+    } catch (e) {
+      console.log("Not a valid command!");
     }
-}
+  } else { // so that it can run in any channel of any server after the invite
+    let tokens = msg.content.split(" ");
+    let command = tokens.shift(); // removes the first element and shifts the array accordingly
+    try {
+      if (command.charAt(0) === "!") {
+        command = command.substring(1);
+        commands[command](msg, tokens); // here tokens is the argument
+        // valid command
+      }
+    } catch (e) {
+      console.log("Not a valid command!");
+    }
+  }
+};
--- commands/help.js
+++ commands/help.js
@@ -1,13 +1,23 @@
-const helpMessage = `
-This is the welcome bot!\n
-Welcome to harshal's server\n
-Some useful commands -\n 
-    !bot - for a welcome message\n
-    !gif - for a random discord gif\n 
-    !gif query_term - for a specific query\n
-    example - !gif cute kitten
-`
+const { MessageEmbed } = require("discord.js");
 
-module.exports = function(msg, args){
-    msg.channel.send(helpMessage);
-}
+module.exports = async function (msg, args) {
+  const embed = new MessageEmbed()
+    .setDescription(
+      "Welcome to harshal's server. Here are some commands that might be of help - "
+    )
+    .addFields([
+      { name: "!help", value: "To know more about this bot" },
+      { name: "!bot", value: "to get a welcome message from the bot" },
+      { name: "!gif", value: "to get a random discord gif" },
+      {
+        name: "!gif query_term",
+        value: "for a specific query gif\nexample - !gif cute kitten",
+      },
+    ])
+    .setTitle("Help")
+    .setColor("GREEN")
+    .setImage(
+      "https://i.pinimg.com/564x/7c/71/01/7c71017e5cf258c5d6e9409a2a8f5900.jpg"
+    );
+  msg.channel.send(embed);
+};
